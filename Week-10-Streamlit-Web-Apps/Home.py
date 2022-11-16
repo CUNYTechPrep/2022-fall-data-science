f8270c503a105c779d8f189ee60cc8134da029de
zd123 <zacharydesario@gmail.com>
2022-11-15 19:29:26 -0500

    making it pretty

--- Week-10-Streamlit-Web-Apps/Home.py
+++ Week-10-Streamlit-Web-Apps/Home.py
@@ -15,32 +15,52 @@ st.header("Welcome to my homepage! 👋")
 # Create three columns 
 col1, col2, col3 = st.columns([1,1,1])
 
+
+# inside of the first column
 with col1:
+
+    # display a picture
     st.image('images/covid-icon.png')
+
+    # display the link to that page.
     st.write('<a href="/covid"> Check out my Covid Dashboard</a>', unsafe_allow_html=True)
     
-    #st.markdown ('[![Foo](http://www.google.com.au/images/nav_logo7.png)](http://google.com.au/)')
-    link_to_covid_page = ''
-
-    #st.markdown('See my covid dashboard here.')
-
+    # display another picture
     st.image('images/friends.png')
+
+    # display another link to that page
     st.write('<a href="https://www.behance.net/datatime">View more pretty data visualizations.</a>', unsafe_allow_html=True)
 
 
+# inside of column 2
 with col2:
+    # display a picture
     st.image('images/covid-map.png')
+
+    # display a link 
     st.write('<a href="/map"> Check out my Interactive Map</a>', unsafe_allow_html=True)    
     
+
+    # same
     st.image('images/github.png')
+    # same
     st.write('<a href="https://github.com/zd123"> View more awesome code on my github.</a>', unsafe_allow_html=True)    
 
+
+
+# inside of column 3
 with col3:
     # st.write('<div style="background:red">asdf </div>', unsafe_allow_html=True)
+    
+    # display a picture
     st.image('https://www.rmg.co.uk/sites/default/files/styles/full_width_1440/public/Atlantic%20liner%20%27Titanic%27%20%28Br%2C%201912%29%20sinking%2C%20bow%20first%2C%201912%2C%20with%20eight%20full%20lifeboats%20nearby%20and%20an%20iceberg%20in%20the%20distance_banner.jpg?itok=fQV6kN3z')
+
+    # display a link to that page
     st.write('<a href="/Titanic">Interact with my ML algorithm.</a>', unsafe_allow_html=True)    
     
+    # same
     st.image('https://i1.sndcdn.com/avatars-000034142709-fv26gu-t500x500.jpg')
+    #same
     st.markdown('<a href="/Bio">Learn more about me as a human :blush:</a>', unsafe_allow_html=True)
 
 
